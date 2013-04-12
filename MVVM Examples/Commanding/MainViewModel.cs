09e1866707c4b1cfd7683aaad80d917dfd33c4ed
Mark Pearl <MarkPearl@gmail.com>
2013-04-12 06:50:56 +0200

    Started on Commanding initial state

---
+++ MVVM Examples/Commanding/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Commanding
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }                    
+    }
+}
