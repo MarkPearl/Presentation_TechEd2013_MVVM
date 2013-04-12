fc03a603b7be72d254463b108f153eed0417d030
Mark Pearl <MarkPearl@gmail.com>
2013-04-12 11:22:12 +0200

    Start Behaviours Project

---
+++ MVVM Examples/Behaviours/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Behaviours
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            var viewModel = new MainViewModel
+            {
+                Name = "Bob"
+            };
+
+            var view = new MainView();
+            view.DataContext = viewModel;
+            view.Show();
+        }
+    }
+}
---
+++ MVVM Examples/Behaviours/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Behaviours
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand DoSomething { get; set; }
+
+        public MainViewModel()
+        {
+            DoSomething = new DelegateCommand(SayHello, CanSayHello);
+        }
+        private void SayHello()
+        {
+            MessageBox.Show("Hello " + Name);
+        }
+
+        private bool CanSayHello()
+        {
+            if (Name == "Bob") return true;
+            return false;
+        }
+    }
+}
