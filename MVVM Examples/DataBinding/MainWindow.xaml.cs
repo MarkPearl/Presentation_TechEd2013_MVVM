7672011741e313336359f2c6eead22c2c54ad479
Mark Pearl <MarkPearl@gmail.com>
2013-04-12 06:16:56 +0200

    Cleaned up DataBinding Initial Example

--- MVVM Examples/DataBinding/MainWindow.xaml.cs
+++ MVVM Examples/DataBinding/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,23 @@ namespace DataBinding
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Person _cusotmer; 
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _cusotmer = new Person
+                {
+                    Name = "Bob", 
+                    Nicknames = new List<string> {"Nickname 1", "Nickname 2"}
+                };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _cusotmer.Name = "New Name";
+            _cusotmer.Nicknames.Add("Another Nickname");
         }
     }
 }
---
+++ MVVM Examples/DataBinding/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DataBinding
+{
+    public class Person
+    {
+        private string _name;
+        
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }       
+
+        public ICollection<string> Nicknames { get; set; }
+    }
+}
