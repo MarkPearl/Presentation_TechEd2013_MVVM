f20b7a4cfd9add3ed479212ffceadb91be85c14f
Mark Pearl <MarkPearl@gmail.com>
2013-04-12 17:03:23 +0200

    Added View First Approach

---
+++ MVVM Examples/ViewFirstApproach/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ViewFirstApproach
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public string Name { get; set; }
+        public DelegateCommand DoSomething { get; set; }
+
+        public MainViewModel()
+        {
+            DoSomething = new DelegateCommand(() => { MessageBox.Show(string.Format("Hello {0}", Name)); }, () => true);
+        }
+    }
+}
---
+++ MVVM Examples/ViewFirstApproach/ViewModelLocator.cs
@@ -0,0 +1,13 @@
+﻿namespace ViewFirstApproach
+{
+    public class ViewModelLocator
+    {
+        public MainViewModel MainViewModel
+        {
+            get
+            {
+                return new MainViewModel {Name = "Mark"};
+            }
+        }
+    }
+}
