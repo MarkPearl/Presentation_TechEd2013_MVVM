80e13d5bbb4b75b90ea95a155f98495e343b9b2e
Mark Pearl <MarkPearl@gmail.com>
2013-04-15 05:51:43 +0200

    More fixes

--- MVVM Examples/DataBinding/Stuff/Dog.cs
+++ MVVM Examples/DataBinding/Stuff/Dog.cs
@@ -1,11 +1,8 @@
 ﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
-namespace DataBinding
+namespace DataBinding.Stuff
 {
-    public class Person
+    public class Dog
     {
         private string _name;
         
--- MVVM Examples/DataBinding/Stuff/Person.cs
+++ MVVM Examples/DataBinding/Stuff/Person.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 
-namespace DataBinding
+namespace DataBinding.Stuff
 {
     public class Person
     {
